1921f0fd6471c8967252f98df0b0f7f6a357776b
Fausti2 <f.delucia@alumno.um.edu.ar>
2025-04-15 14:31:11 -0300

    hice la funcion de comparacion de caracteres

--- tests/test_palindrome.py
+++ tests/test_palindrome.py
@@ -1,5 +1,5 @@
 import unittest
-#from src.palindrome import is_palindrome 
+from src.palindrome import is_palindrome 
 class testpalindrome (unittest.TestCase):
     def test_is_palindrome(self):
         self.assertTrue(is_palindrome("madam"))
@@ -8,7 +8,7 @@ class testpalindrome (unittest.TestCase):
         self.assertFalse(is_palindrome("Mundo")) 
     def test_frases_palindrome(self):
         self.assertTrue(is_palindrome("La ruta natural"))
-        self.assertTrue(is_palindrome("Amo la pacífica paloma"))
+        self.assertTrue(is_palindrome("Amo la pacifica paloma"))
         self.assertTrue(is_palindrome("La ruta natural"))
     def test_frases_no_palindrome(self):
         self.assertFalse(is_palindrome("La ruta no es natural"))
@@ -16,9 +16,10 @@ class testpalindrome (unittest.TestCase):
         self.assertFalse(is_palindrome(" me gusta la computadora "))
     def test_casos_edge(self):
         self.assertTrue(is_palindrome("A Santa at NASA"))
-        self.assertTrue(is_palindrome("¿Cómo es tu nombre?"))
+        self.assertFalse(is_palindrome("Como es tu nombre?"))
         self.assertTrue(is_palindrome("No lemon, no melon"))
-        self.assertTrue(is_palindrome("5458989"))
+        self.assertFalse(is_palindrome("5458989"))
     
+
 if __name__ == '__main__':
     unittest.main() 
